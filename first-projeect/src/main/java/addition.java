00a2ca3345814eedc0531b3c0a5603650394be1a
Ravindra <Ravindra@DESKTOP-50EU6F9.lan>
2021-05-24 13:43:30 -0400

    my first commit push

---
+++ first-projeect/src/main/java/addition.java
@@ -0,0 +1,15 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 20;
+		int c= 30;
+		int x = (a + b) - c;
+		
+		System.out.println("result:" +x);
+		System.out.println("int a:" +a);
+
+	}
+
+}
---
+++ first-projeect/src/main/java/learn.java
@@ -0,0 +1,14 @@
+
+public class learn {
+	
+ public static void main(String[] args) {
+	 
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+	
+   System.out.println("Sum of the numbers:" +c) ;
+   System.out.println("y:" +c);
+		
+	}
+}
